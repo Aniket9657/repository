fb4f87393e847e5aa4b447c4468a8d8192c77afa
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-04-19 23:50:50 +0530

    added max practice stuff)

--- Swap.java
+++ Swap.java
@@ -9,6 +9,8 @@ public class Swap {
 		swap(arr,1,3);
 		
 		System.out.println(Arrays.toString(arr));
+		
+		System.out.println(Max(arr));
 	}
      static void swap(int [] arr ,int index , int index1  )
 {
@@ -16,4 +18,21 @@ public class Swap {
 	arr[index] = arr[index1];
 	arr[index1]=temp;
 }
+     
+     
+     
+     public static int Max(int[] arr )
+     {
+    	 int max =0;
+    	 for (int i=0; i<arr.length;i++)
+    	 {
+    		 if (arr[i]>max)
+    		 {
+    			 max=arr[i];
+    			 
+    		 }
+    	 }
+    	 return max;
+    	 
+     }
 }
