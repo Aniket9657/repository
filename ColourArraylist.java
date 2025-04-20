42b259467ffbd578c72b7402e6c2582d0af4e68c
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-04-20 11:04:22 +0530

    Colour List

---
+++ ColourArraylist.java
@@ -0,0 +1,34 @@
+package com.Collection;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ColourArraylist {
+	
+	public static void main (String[] args)
+	{
+		Scanner in  =  new Scanner(System.in);
+		
+		
+		ArrayList<String> Clist = new ArrayList<>(5 );
+		
+		
+		System.out.println("Enter the number of colour needed");
+	
+		int n=in.nextInt();
+		System.out.println("Enter the Fruits");
+		
+		for (int i =0; i<n;i++)
+		{
+			String Colour =in.next();
+			Clist.add(Colour);
+		}
+		
+		
+		System.out.println("colours in the list "+Clist);
+		
+		in.close();
+		
+	}
+
+}
