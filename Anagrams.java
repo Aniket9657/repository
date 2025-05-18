18a563b6af15abcbd50ad518da4076b018554e12
Aniket9657 <124755066+Aniket9657@users.noreply.github.com>
2025-05-18 22:49:08 +0530

    Anagram

---
+++ Anagrams.java
@@ -0,0 +1,17 @@
+package com.Collection;
+
+public class Anagrams {
+	public static void main(String[]args)
+	{
+	String s1 = "Ant";
+	String s2= "tan";
+	if (s1.length()==s2.length())
+	{
+	for(int i=0;i<s1.length();i++)
+	{
+	s1.charAt(i)==s2.charAt(i);
+		
+
+		
+	}
+}
